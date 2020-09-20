b5574c87a3f1803e8f3dc1e062ad3fadaba2ba62
tonywilliams24 <58370872+tonywilliams24@users.noreply.github.com>
2020-09-20 15:45:00 -0700

    09/20/2020

---
+++ src/Strings/CompleteTheWord/Challenge.java
@@ -0,0 +1,16 @@
+package Strings.CompleteTheWord;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Challenge {
+    public static boolean canComplete(String initial, String word) {
+        Pattern initialPattern;
+        Matcher wordMatcher;
+        String regex = "\\w*";
+        for(int i=0; i<initial.length();i++) regex = regex.concat("[" + initial.charAt(i) + "]" + "\\w*");
+        initialPattern = Pattern.compile(regex);
+        wordMatcher = initialPattern.matcher(word);
+        return wordMatcher.find();
+    }
+}
